1457c2b833fd65344465000cd8c76664dd7376fa
Minh Tran <minhtran@Minhs-MacBook-Pro.local>
2023-01-30 16:37:24 +0700

    add new constant

--- src/mosap/__init__.py
+++ src/mosap/__init__.py
@@ -1,5 +1,18 @@
 __version__ = "0.0.1"
 
+PROTEIN_EXP_DIR = "DecodedExpMasks"
+PROTEIN_EXP_ALT_DIR = "DecodedProteinExp"
+PROTEIN_EXP_ALT_ALT_DIR = "LOD_Overlay"
+
+ALPHA_UM_PER_PX = 0.168
+ALPHA_MM_PER_PX = ALPHA_UM_PER_PX/1000
+ALPHA_PX_PER_MM = 1/ALPHA_MM_PER_PX
+
+UM_PER_PX = 0.18
+MM_PER_PX = UM_PER_PX/1000
+PX_PER_MM = 1/MM_PER_PX
+
+
 from ._reader import napari_get_reader
 from ._widget import ExampleQWidget, example_magic_widget
 
