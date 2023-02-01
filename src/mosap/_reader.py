25e2543d6dff93aa2edd9374a9069f7ba9313acd
Minh Tran <minhtran@Minhs-MacBook-Pro.local>
2023-02-01 15:18:56 +0700

    update reader, nosap reads folder function

--- src/mosap/_reader.py
+++ src/mosap/_reader.py
@@ -6,7 +6,13 @@ implement multiple readers or even other plugin contributions. see:
 https://napari.org/stable/plugins/guides.html?#readers
 """
 import numpy as np
+import os
+from mosap.mosap import SpatialOmics
+from mosap._widget import MultiOmicRegistrationWidget, Transcript_Selection_Widget 
+from mosap.utils.file_listing import get_files_in_dir_recursively 
 
+from skimage.io import imread
+import napari
 
 def napari_get_reader(path):
     """A basic implementation of a Reader contribution.
@@ -22,18 +28,17 @@ def napari_get_reader(path):
         If the path is a recognized format, return a function that accepts the
         same path or list of paths, and returns a list of layer data tuples.
     """
-    if isinstance(path, list):
-        # reader plugins may be handed single path, or a list of paths.
-        # if it is a list, it is assumed to be an image stack...
-        # so we are only going to look at the first file.
-        path = path[0]
-
-    # if we know we cannot read the file, we immediately return None.
-    if not path.endswith(".npy"):
-        return None
+    # Load metadata
+    if path.endswith("_measurement.txt") or path.endswith("_measurement.csv"):
+        return read_metadata_function
+    # Allow parent directory or AnnData file to be opened
+    # if not (path.endswith(".h5ad") or \
+    #     os.path.exists(os.path.join(path, "images"))):
+    #     return None
 
     # otherwise we return the *function* that can read ``path``.
     return reader_function
+
 
 
 def reader_function(path):
@@ -58,15 +63,58 @@ def reader_function(path):
         layer. Both "meta", and "layer_type" are optional. napari will
         default to layer_type=="image" if not provided
     """
-    # handle both a string and a list of strings
-    paths = [path] if isinstance(path, str) else path
-    # load all files into array
-    arrays = [np.load(_path) for _path in paths]
-    # stack arrays into single array
-    data = np.squeeze(np.stack(arrays))
+    if isinstance(path, list):
+        # Only want RunSummary directory if list of paths is present
+        path = path[0]
+    # print("Read function with Mosap",path)
+    image = get_files_in_dir_recursively(path,'*tif')
+    # print(image)
+    # 1/0
+    image = image[0]
+    viewer = napari.current_viewer()
+    # 1/0
+    # gem = Gemini(path, viewer=viewer)
+    # viewer.window.add_dock_widget(MultiOmicRegistrationWidget(viewer),
+    #     area='right')
+    if os.path.exists(image):
+        data = imread(image)
+    
+    
+    # labels layer added in Gemini instance initialization
+    print('Loaded folder and created Mosap instance')
+    # return [(None,)]
+    return [
+        (data, {"name": image.split('/')[-1]}, "image"),
+    ]
 
-    # optional kwargs for the corresponding viewer.add_* method
-    add_kwargs = {}
+def read_metadata_function(path):
+    """Take a path or list of paths and return a list of LayerData tuples.
 
-    layer_type = "image"  # optional, default is "image"
-    return [(data, add_kwargs, layer_type)]
+    Readers are expected to return data as a list of tuples, where each tuple
+    is (data, [add_kwargs, [layer_type]]), "add_kwargs" and "layer_type" are
+    both optional.
+
+    Parameters
+    ----------
+    path : str or list of str
+        Path to file, or list of paths.
+
+    Returns
+    -------
+    layer_data : list of tuples
+        A list of LayerData tuples where each tuple in the list contains
+        (data, metadata, layer_type), where data is a numpy array, metadata is
+        a dict of keyword arguments for the corresponding viewer.add_* method
+        in napari, and layer_type is a lower-case string naming the type of layer.
+        Both "meta", and "layer_type" are optional. napari will default to
+        layer_type=="image" if not provided
+    """
+    if isinstance(path, list):
+        path = path[0]
+    
+    viewer = napari.current_viewer()
+    for w in viewer.window._dock_widgets.values():
+        if isinstance(w.widget(), GeminiQWidget):
+            w.widget().update_metadata(path)
+            break
+    return [(None,)]
--- src/mosap/_widget.py
+++ src/mosap/_widget.py
@@ -7,15 +7,19 @@ see: https://napari.org/stable/plugins/guides.html?#widgets
 Replace code below according to your needs.
 """
 from typing import TYPE_CHECKING
-
+from typing import Any, Union, Optional
 from magicgui import magic_factory
 from qtpy.QtWidgets import QHBoxLayout, QPushButton, QWidget, QVBoxLayout, QGroupBox
-from qtpy.QtWidgets import QLabel, QComboBox, QGridLayout, QFileDialog
+from qtpy.QtWidgets import QLabel, QComboBox, QGridLayout, QFileDialog,QProgressBar
 from mosap.mosap  import SpatialOmics
+import os
 if TYPE_CHECKING:
     import napari
 from napari.utils.notifications import show_info
 
+
+# import SimpleITK as sitk
+from mosap.registration import save_transformation_model, read_transformation_model
 class MultiOmicRegistrationWidget(QWidget):
     TRANSFORMATIONS = {
         "translation": "Translation",
@@ -68,6 +72,16 @@ class MultiOmicRegistrationWidget(QWidget):
             self.btn_transform.setEnabled(self.tmats is not None)
             self.btn_register_transform.setEnabled(True)
     
+    
+
+    def _save_tmat(self, filename:str):
+        save_transformation_model(self.tmats, filename)
+
+    def _load_tmat(self, filename:str):
+        transformer = read_transformation_model(filename)
+        self.tmats = transformer
+
+    
     def _btn_save_tmat_onclick(self, value: bool):
         fname = QFileDialog.getSaveFileName(
             self, "Save transformationer to file", filter="*.tfm"
@@ -79,7 +93,7 @@ class MultiOmicRegistrationWidget(QWidget):
         fname = QFileDialog.getOpenFileName(
             self, "Open transformation matrix file", filter="*.tfm"
         )[0]
-
+        # display transformer
         try:
             self._load_tmat(fname)
         except Exception:
@@ -90,9 +104,31 @@ class MultiOmicRegistrationWidget(QWidget):
         self.btn_tmat_save.setEnabled(True)
 
         # if image is open we can now transform it
-        if self.image.currentData() is not None:
+        if self.moving_image.currentText() is not None:
             self.btn_transform.setEnabled(True)
-        show_info("Loaded transformation matrices from file")
+        show_info("Loaded transformer from file")
+
+    def _btn_register_onclick(self, value: bool):
+        self._change_button_accessibility(False)
+        try:
+            self._run("register")
+        finally:
+            show_info("Registered image")
+            self._change_button_accessibility(True)
+
+    def _btn_transform_onclick(self, value: bool):
+        self._change_button_accessibility(False)
+        # run transform
+        try:
+            self._run("transform")
+        finally:
+            show_info("Transformed image")
+            self._change_button_accessibility(True)
+
+    def _change_button_accessibility(self, value: bool):
+        self.btn_register.setEnabled(value)
+        self.btn_transform.setEnabled(value)
+        
 
     def createImageOptionWidget(self):
         groupBox = QGroupBox(self, title="Images and Transformers")
@@ -103,10 +139,13 @@ class MultiOmicRegistrationWidget(QWidget):
         tooltip_transformation = "Type of applied transformation."
         self.moving_image = QComboBox(groupBox)
         self.moving_image.setToolTip(tooltip_message_moving_image)
-        self.moving_image.addItems(['mov1','mov2','mov3','mov4','mov5'])
+        self.moving_image.addItems(['mov1', 'mov2'])
+        self.moving_image.currentIndexChanged.connect(
+            self._move_image_onchange
+        )
         self.ref_image = QComboBox(groupBox)
         self.ref_image.setToolTip(tooltip_message_reference_image)
-        self.ref_image.addItems(['ref1', 'ref2', 'ref3', 'ref4'])
+        self.ref_image.addItems(['ref1','ref2'])
         # self.image.currentIndexChanged.connect(self._image_onchange)
         reg_img_label = QLabel("Register Image")
         reg_img_label.setToolTip(tooltip_message_moving_image)
@@ -122,7 +161,13 @@ class MultiOmicRegistrationWidget(QWidget):
             self.registration_model.addItem(v, k)
             
         self.registration_model.setCurrentText("Affine and BSpline")
+        self.registration_model.currentIndexChanged.connect(
+            self._transformation_onchange
+        )
         
+        self.ref_image.currentIndexChanged.connect(
+            self._ref_image_onchange
+        )
         tmat_label = QLabel("Transformer file")
         
         tmat_label.setToolTip('Registration transformers can be loaded and saved from files')
@@ -139,6 +184,13 @@ class MultiOmicRegistrationWidget(QWidget):
         # Register and transform functional button
         self.btn_register = QPushButton("Register")
         self.btn_transform = QPushButton("Transform")
+        self.btn_register.clicked.connect(self._btn_register_onclick)
+        self.btn_transform.clicked.connect(self._btn_transform_onclick)
+
+        self.pbar_label = QLabel()
+        self.pbar = QProgressBar()
+        self.pbar.setVisible(False)
+        
 
         grid = QGridLayout()
         grid.addWidget(reg_img_label, 1,0)
@@ -152,15 +204,259 @@ class MultiOmicRegistrationWidget(QWidget):
         grid.addWidget(self.btn_tmat_save, 4,2)
         grid.addWidget(curr_tmat_label, 5,0)
         grid.addWidget(self.status, 5,1)
+        grid.addWidget(self.btn_register, 6,0)
+        grid.addWidget(self.btn_transform, 6,1)
+        grid.addWidget(self.pbar_label, 7,0)
+        grid.addWidget(self.pbar, 7,1)
         # vbox.addWidget(btn)
         vbox.addLayout(grid)
         groupBox.setLayout(vbox)
         self.layout().addWidget(groupBox)
         
         self.layout().addWidget(groupBox)
-@magic_factory
-def Transcript_Selection_Widget(img_layer: "napari.layers.Image"):
-    print(f"you have selected Transcript widget {img_layer}")
+
+    def _transformation_onchange(self, value: str):
+        def without(d, key):
+            new_d = d.copy()
+            new_d.pop(key)
+            return new_d
+        print(self.registration_model.currentData() == "bilinear")
+        print(self.registration_model.currentData())
+
+    def _move_image_onchange(self, value: str):
+        print("napari has", len(self.viewer.layers), "layers")
+        print('moving image selected ', self.moving_image.currentText())
+
+    def _ref_image_onchange(self,  value: str):
+        print("napari has", len(self.viewer.layers), "layers")
+        print('reference image selected ', self.ref_image.currentText())
+
+        # if self.registration_model.currentData() == "bilinear":
+        #     refs = without(self.REFERENCES, "previous")
+        # else:
+        #     refs = self.REFERENCES
+
+        # self.reference.clear()
+        # for k, v in refs.items():
+        #     self.reference.addItem(v, k)
+    
+    def _run(self, action):
+        import numpy as np
+        
+
+        # transformations = {
+        #     "translation": StackReg.TRANSLATION,
+        #     "rigid_body": StackReg.RIGID_BODY,
+        #     "scaled_rotation": StackReg.SCALED_ROTATION,
+        #     "affine": StackReg.AFFINE,
+        #     "bilinear": StackReg.BILINEAR,
+        # }
+
+        transformation = self.registration_model.currentData()
+        print('Perform SITK', transformation)
+        # moving_average = (
+        #     self.moving_average.value()
+        #     if self.perform_moving_average.isChecked()
+        #     else 1
+        # )
+
+        # n_frames = self.n_frames.value()
+        # reference = self.reference.currentData()
+
+        # if reference not in self.REFERENCES:
+        #     raise ValueError(f'Unknown reference "{reference}"')
+
+        # image = self.image.currentData()
+
+        # self.pbar.setMaximum(image.shape[0] - 1)
+
+        # def hide_pbar():
+        #     self.pbar_label.setVisible(False)
+        #     self.pbar.setVisible(False)
+
+        # def show_pbar():
+        #     self.pbar_label.setVisible(True)
+        #     self.pbar.setVisible(True)
+
+        # @thread_worker(connect={"returned": hide_pbar}, start_thread=False)
+        # def _register_stack(image) -> ImageData:
+
+        #     sr = StackReg(transformations[transformation])
+
+        #     axis = 0
+
+        #     if action in ["register", "register_transform"]:
+        #         image_reg = image
+        #         idx_start = 1
+
+        #         if moving_average > 1:
+        #             idx_start = 0
+        #             size = [0] * len(image_reg.shape)
+        #             size[axis] = moving_average
+        #             image_reg = running_mean(
+        #                 image_reg, moving_average, axis=axis
+        #             )
+
+        #         tmatdim = 4 if transformation == "bilinear" else 3
+
+        #         tmats = np.repeat(
+        #             np.identity(tmatdim).reshape((1, tmatdim, tmatdim)),
+        #             image_reg.shape[axis],
+        #             axis=0,
+        #         ).astype(np.double)
+
+        #         if reference == "first":
+        #             ref = np.mean(
+        #                 image_reg.take(range(n_frames), axis=axis), axis=axis
+        #             )
+        #         elif reference == "mean":
+        #             ref = image_reg.mean(axis=0)
+        #             idx_start = 0
+        #         elif reference == "previous":
+        #             pass
+        #         else:  # pragma: no cover - can't be reached due to check above
+        #             raise ValueError(f'Unknown reference "{reference}"')
+
+        #         self.pbar_label.setText("Registering...")
+
+        #         iterable = range(idx_start, image_reg.shape[axis])
+
+        #         for i in iterable:
+        #             slc = [slice(None)] * len(image_reg.shape)
+        #             slc[axis] = i
+
+        #             if reference == "previous":
+        #                 ref = image_reg.take(i - 1, axis=axis)
+
+        #             tmats[i, :, :] = sr.register(
+        #                 ref, simple_slice(image_reg, i, axis)
+        #             )
+
+        #             if reference == "previous" and i > 0:
+        #                 tmats[i, :, :] = np.matmul(
+        #                     tmats[i, :, :], tmats[i - 1, :, :]
+        #                 )
+
+        #             yield i - idx_start + 1
+
+        #         self.tmats = tmats
+        #         image_name = self.image.itemText(self.image.currentIndex())
+        #         transformation_name = self.TRANSFORMATIONS[transformation]
+        #         self.status.setText(
+        #             f'Registered "{image_name}" [{transformation_name}]'
+        #         )
+        #         self.btn_tmat_save.setEnabled(True)
+
+        #     if action in ["transform", "register_transform"]:
+        #         tmats = self.tmats
+
+        #         # transform
+
+        #         out = image.copy().astype(np.float)
+
+        #         self.pbar_label.setText("Transforming...")
+        #         yield 0  # reset pbar
+
+        #         for i in range(image.shape[axis]):
+        #             slc = [slice(None)] * len(out.shape)
+        #             slc[axis] = i
+        #             out[tuple(slc)] = sr.transform(
+        #                 simple_slice(image, i, axis), tmats[i, :, :]
+        #             )
+        #             yield i
+
+        #         # convert to original dtype
+        #         if np.issubdtype(image.dtype, np.integer):
+        #             out = to_int_dtype(out, image.dtype)
+
+        #         return out
+
+        # def on_yield(x):
+        #     self.pbar.setValue(x)
+
+        # def on_return(img):
+        #     if img is None:
+        #         return
+        #     image_name = self.image.itemText(self.image.currentIndex())
+
+        #     transformation_name = self.transformation.itemText(
+        #         self.transformation.currentIndex()
+        #     )
+
+        #     layer_name = f"Registered {image_name} ({transformation_name})"
+        #     self.viewer.add_image(data=img, name=layer_name)
+
+        # self.worker = _register_stack(image)
+
+        # if running_coverage:
+            
+        #     patch_worker_for_coverage(self.worker)
+        # self.worker.yielded.connect(on_yield)
+        # self.worker.returned.connect(on_return)
+        # self.worker.start()
+
+        # show_pbar()
+
+
+# @magic_factory
+class Transcript_Selection_Widget(QWidget):
+    def __init__(self, napari_viewer):
+        super().__init__()
+        self.viewer = napari_viewer
+        # self.so = so
+        vbox_layout = QVBoxLayout(self)
+        vbox_layout.setContentsMargins(9, 9, 9, 9)
+
+        self.setLayout(vbox_layout)
+        # btn = QPushButton("Click me!")
+        # btn.clicked.connect(self._on_click)
+
+        # self.setLayout(QHBoxLayout())
+        # self.layout().addWidget(btn)
+        self.createTranscriptsWidget()
+    def createTranscriptsWidget(self):
+        groupBox = QGroupBox(self, title="RNA Transcripts")
+
+        btn = QPushButton("Add layer")
+        btn.clicked.connect(self._on_select_rna_click)
+
+        boxp = QComboBox(groupBox)
+        # boxp.addItems(self.gem.target_labels())
+        boxp.addItems(['1','2','3','4','5', '6'])
+        colors = ['white', 'red', 'green', 'blue',
+            'magenta', 'yellow', 'cyan']
+        boxc = QComboBox(groupBox)
+        boxc.addItems(colors)
+
+        self.targetsComboBox = boxp
+        self.targetsComboBox.currentIndexChanged.connect(
+            self._selected_number_target
+        )
+        self.colorsComboBox = boxc
+
+        vbox = QVBoxLayout(groupBox)
+        grid = QGridLayout()
+        lbl_numb_target = QLabel('Number of targets:')
+        lbl_numb_target.setToolTip('Select the number of transcript to display')
+
+        grid.addWidget(lbl_numb_target, 1, 0)
+        grid.addWidget(self.targetsComboBox, 1, 1)
+        grid.addWidget(QLabel('color:'), 2, 0)
+        grid.addWidget(self.colorsComboBox, 2, 1)
+        vbox.addLayout(grid)
+        vbox.addWidget(btn)
+        groupBox.setLayout(vbox)
+
+        self.layout().addWidget(groupBox)
+
+    def _on_select_rna_click(self):
+        print('Plot RNA expression function')
+        # self.gem.plot_transcripts(gene=self.targetsComboBox.currentText(),
+        # color=self.colorsComboBox.currentText())
+    
+    def _selected_number_target(self):
+        print('Selected:', self.targetsComboBox.currentData())
+
 
 
 # Uses the `autogenerate: true` flag in the plugin manifest
