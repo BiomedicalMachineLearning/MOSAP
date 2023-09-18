3f25de7d4141bafe2b83754dea772b1d955e9dca
Minh Tran <minhtran@Minhs-MacBook-Pro.local>
2023-09-18 22:32:14 +1000

    update plotting network function and neighbourhood enrichment anlysis functions

--- src/mosap/neighborhood.py
+++ src/mosap/neighborhood.py
@@ -1,8 +1,161 @@
+import os
 import networkx as nx
 import numpy as np
 import pandas as pd
+import time
+from mosadata import MOSADATA
+from networkx import Graph
+from pandas.api.types import CategoricalDtype
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+class Interactions:
+    """
+    Estimator to quantify interaction strength between different species in the sample.
+    """
+    VALID_PREDICTION_TYPES = ['pvalue', 'observation', 'diff']
+
+    def __init__(self, mosadata:MOSADATA, sample: str, attr: str = 'meta_id', mode: str = 'classic', 
+                 n_permutations: int = 500,
+                 random_seed=None, alpha: float = .01, graph_key: str = 'knn'):
+        """Estimator to quantify interaction strength between different species in the sample.
+        """
+
+        self.data = mosadata
+        self.spl: str = sample
+        self.graph_key = graph_key
+        self.g: Graph = mosadata.G[sample][graph_key]
+        self.attr: str = attr
+        self.data: pd.Series = mosadata.obs[sample][attr]
+        self.mode: str = mode
+        self.n_perm: int = int(n_permutations)
+        self.random_seed = random_seed if random_seed else mosadata.random_seed
+        self.rng = np.random.default_rng(random_seed)
+        self.alpha: float = alpha
+        self.fitted: bool = False
+
+        # set dtype categories of data to attributes that are in the data
+        self.data = self.data.astype(CategoricalDtype(categories=self.data.unique(), ordered=False))
+
+
+    def fit(self, prediction_type:str, try_load: bool = True) -> None:
+        if prediction_type not in self.VALID_PREDICTION_TYPES:
+            raise ValueError(
+                f'invalid `prediction_type` {prediction_type}. Available modes are {self.VALID_PREDICTION_TYPES}')
+
+        self.prediction_type = prediction_type
+        self.mode == 'proportion'
+        relative_freq, observed = True, False
+        node_interactions = get_node_interactions(self.g, self.data)
+        obs_interaction = get_interaction_score(node_interactions, relative_freq=relative_freq, observed=observed)
+        self.obs_interaction = obs_interaction.set_index(['source_ct', 'target_ct'])
+
+        if not self.prediction_type == 'observation':
+            if try_load:
+                if os.path.isdir(self.path) and self.h0_file in os.listdir(self.path):
+                    logging.info(
+                        f'loading h0 for {self.spl}, graph type {self.graph_key} and mode {self.mode}')
+                    self.h0 = pd.read_pickle(os.path.join(self.path, self.h0_file))
+            # if try_load was not successful
+            if self.h0 is None:
+                logging.info(
+                    f'generate h0 for {self.spl}, graph type {self.graph_key} and mode {self.mode} and attribute {self.attr}')
+                self.generate_h0(relative_freq=relative_freq, observed=observed, save=True)
+
+        self.fitted = True
+
+    def predict(self) -> pd.DataFrame:
+        """Predict interactions strengths of observations.
+
+        Returns: A dataframe with the interaction results.
+
+        """
+        if self.prediction_type == 'observation':
+            return self.obs_interaction
+        elif self.prediction_type == 'pvalue':
+            # TODO: Check p-value computation
+            data_perm = pd.concat((self.obs_interaction, self.h0), axis=1)
+            data_perm.fillna(0, inplace=True)
+            data_pval = pd.DataFrame(index=data_perm.index)
+
+            # see h0_models_analysis.py for alterantive p-value computation
+            data_pval['score'] = self.obs_interaction.score
+            data_pval['perm_mean'] = data_perm.apply(lambda x: np.mean(x[1:]), axis=1, raw=True)
+            data_pval['perm_std'] = data_perm.apply(lambda x: np.std(x[1:]), axis=1, raw=True)
+            data_pval['perm_median'] = data_perm.apply(lambda x: np.median(x[1:]), axis=1, raw=True)
+
+            data_pval['p_gt'] = data_perm.apply(lambda x: np.sum(x[1:] >= x[0]) / self.n_perm, axis=1, raw=True)
+            data_pval['p_lt'] = data_perm.apply(lambda x: np.sum(x[1:] <= x[0]) / self.n_perm, axis=1, raw=True)
+            data_pval['perm_n'] = data_perm.apply(lambda x: self.n_perm, axis=1, raw=True)
+
+            data_pval['p'] = data_pval.apply(lambda x: x.p_gt if x.p_gt <= x.p_lt else x.p_lt, axis=1)
+            data_pval['sig'] = data_pval.apply(lambda x: x.p < self.alpha, axis=1)
+            data_pval['attraction'] = data_pval.apply(lambda x: x.p_gt <= x.p_lt, axis=1)
+            data_pval['sigval'] = data_pval.apply(lambda x: np.sign((x.attraction - .5) * x.sig), axis=1)
+            return data_pval
+        elif self.prediction_type == 'diff':
+            data_perm = pd.concat((self.obs_interaction, self.h0), axis=1)
+            data_perm.fillna(0, inplace=True)
+            data_pval = pd.DataFrame(index=data_perm.index)
+
+            # see h0_models_analysis.py for alterantive p-value computation
+            data_pval['score'] = self.obs_interaction.score
+            data_pval['perm_mean'] = data_perm.apply(lambda x: np.mean(x[1:]), axis=1, raw=True)
+            data_pval['perm_std'] = data_perm.apply(lambda x: np.std(x[1:]), axis=1, raw=True)
+            data_pval['perm_median'] = data_perm.apply(lambda x: np.median(x[1:]), axis=1, raw=True)
+
+            data_pval['diff'] = (data_pval['score'] - data_pval['perm_mean'])
+            return data_pval
+
+        else:
+            raise ValueError(
+                f'invalid `prediction_type` {self.prediction_type}. Available modes are {self.VALID_PREDICTION_TYPES}')
+
+    def generate_h0(self, relative_freq, observed, save=True):
+        # usually get called if the paramater is prediction_type=observation
+        # This function work similar to squidpy permutation but improved with the source cell type and target cell type 
+        connectivity = get_node_interactions(self.g).reset_index(drop=True)
+
+        res_perm, durations = [], []
+        for i in range(self.n_perm):
+            tic = time.time()
+
+            data = permute_labels(self.data, self.rng)
+            source_ct = data.loc[connectivity.source].values.ravel()
+            target_ct = data.loc[connectivity.target].values.ravel()
+
+            # create pd.Series and node_interaction pd.DataFrame
+            source_ct = pd.Series(source_ct, name='source_ct', dtype=self.data.dtype)
+            target_ct = pd.Series(target_ct, name='target_ct', dtype=self.data.dtype)
+            df = pd.concat((connectivity, source_ct, target_ct), axis=1)
+
+            # get interaction count
+            perm = get_interaction_score(df, relative_freq=relative_freq, observed=observed)
+            perm['permutation_id'] = i
+
+            # save result
+            res_perm.append(perm)
+
+            # stats
+            toc = time.time()
+            durations.append(toc - tic)
+
+            if (i + 1) % 10 == 0:
+                print(f'{time.asctime()}: {i + 1}/{self.n_perm}, duration: {np.mean(durations):.2f}) sec')
+        print(
+            f'{time.asctime()}: Finished, duration: {np.sum(durations) / 60:.2f} min ({np.mean(durations):.2f}sec/it)')
+
+        h0 = pd.concat(res_perm)
+        self.h0 = pd.pivot(h0, index=['source_ct', 'target_ct'], columns='permutation_id', values='score')
+
+
+def permute_labels(data, rng: np.random.Generator):
+    return pd.Series(rng.permutation(data), index=data.index)
 
 def get_node_interactions(g: nx.Graph, data: pd.Series = None):
+    ### count the interaction by unique cell type in the grph
+    ###
     source, neighs = [], []
     for i in g.nodes:
         if len(g[i]) > 0:  # some nodes might have no neighbors
@@ -11,15 +164,17 @@ def get_node_interactions(g: nx.Graph, data: pd.Series = None):
     # convert neighbourhood network to pandas dataframe
     node_interactions = pd.DataFrame({'source': source, 'target': neighs}).explode('target')
     if data is not None:
-        # iterate through the netwok and get the count of edges connected to source node and label node
+        # iterate through the network and get the count of edges connected to source node and label node
         node_interactions['source_ct'] = data.loc[node_interactions.source].values
         node_interactions['target_ct'] = data.loc[node_interactions.target].values
 
     return node_interactions
 
 
-def get_interaction_score(interactions, relative_freq=False, observed=False):
+def get_interaction_score(interactions, observed=False):
     # function to calculate the interaction score by counting the edges and grouped cell type
+    # this function takes the paramater interactions (dataframe) as the input, should have 4 columns
+    # source, target, source_ct (count of source ct), target_ct (count of target neighbour cell type)
     source_label = interactions[['source', 'source_ct']].drop_duplicates().set_index('source')
     source_label = source_label.squeeze()
 
--- src/mosap/utils/plot.py
+++ src/mosap/utils/plot.py
@@ -4,11 +4,51 @@ from pathlib import Path
 from typing import Any, Union, Optional  # Meta
 from typing import Iterable, Sequence, Mapping, MutableMapping  # Generic ABCs
 from typing import Tuple, List
-import tifffile
+# import tifffile
 import re
 import logging
 from mosap.mosadata import MOSADATA
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
 
+# default
+
+dpi=300
+ax_pad = 10
+label_fontdict = {'size': 7}
+title_fontdict = {'size': 10}
+cbar_inset = [1.02, 0, .0125, .96]
+cbar_titel_fontdict = {'size': 7}
+cbar_labels_fontdict = {'size': 7}
+
+def scatter_cell_plot(mosadata:MOSADATA,sample, coordx:str, coordy:str, color_type:str, figsize=(15,15), size=1.0, title=None, color_mapper=None):
+    fig, axs = plt.subplots(1, 1, figsize=figsize)
+    cell_type_color_mapper = sns.color_palette("Set2", len(mosadata.obs[sample][color_type].unique()))
+    if color_mapper is None: 
+        color_mapper = dict()
+        for index, color in enumerate(cell_type_color_mapper):
+    #         print(color)
+            color_mapper[sorted(list(mosadata.obs[sample][color_type].unique()))[index]] = color
+    
+    legend_patches = list()
+    for key, value in color_mapper.items():
+        tmp_patch = mpatches.Patch(color=value, label=key)
+        legend_patches.append(tmp_patch)
+
+    for index, row in mosadata.obs[sample].iterrows():
+        axs.scatter(row[coordx],row[coordy],
+                    color=color_mapper[row[color_type]],
+                    linewidth=1.0)
+#     axs.imshow(rotated_image)
+    if title is None:
+        title = color_type
+        
+    axs.legend(handles=legend_patches,
+               bbox_to_anchor=(0.97, 1.0),
+               loc=2, title=title, prop={'size': 8})
+    return axs, fig
 
 def extract_physical_dimension(self, ome_tiff_path):
         """ A function to load the original OME tiff to extract micron resolution and pixel conversion"""
@@ -39,24 +79,277 @@ def convert_micron2pixel(x_micron, micron_dim, scale_dim):
     """
     return (x_micron*scale_dim/micron_dim)
 
-def convert_unit_micron2pixel(spomic:MOSADATA, original_width_micron, original_height_micron, 
+def convert_unit_micron2pixel(mosadata:MOSADATA, sample, original_width_micron, original_height_micron, 
                                   X_col:Optional[str]=None,Y_col:Optional[str]=None):
         """ Running the conversion of the unit from micron unit to pixel image """
         """ In case you do not have the conversion unit please use the original OME.tiff extract_physical_dimension"""
-        if spomic.original_coord_unit != spomic.ref_image_unit:
-            logging.warning(f'Converting {spomic.original_coord_unit} to {spomic.ref_image_unit}')
+        if mosadata.original_coord_unit != mosadata.ref_image_unit:
+            logging.warning(f'Converting {mosadata.original_coord_unit} to {mosadata.ref_image_unit}')
             
         if X_col:
-            spomic.obs['X_px'] = convert_micron2pixel(spomic.obs[X_col], original_width_micron, 
-                                                      spomic.ref_image.shape[1])
+            mosadata.obs[sample]['X_px'] = convert_micron2pixel(mosadata.obs[sample][X_col], original_width_micron, 
+                                                      mosadata.obs[sample].ref_image.shape[1])
         else:
-            spomic.obs['X_px'] = convert_micron2pixel(spomic.obs[spomic.__centroid_X], original_width_micron, 
-                                                      spomic.ref_image.shape[1])
+            mosadata.obs[sample]['X_px'] = convert_micron2pixel(mosadata.obs[mosadata.__centroid_X], original_width_micron, 
+                                                      mosadata.obs[sample].ref_image.shape[1])
         
         if Y_col:
-            spomic.obs['Y_px'] = convert_micron2pixel(spomic.obs[Y_col], original_height_micron, 
-                                                      spomic.ref_image.shape[0])
+            mosadata.obs[sample]['Y_px'] = convert_micron2pixel(mosadata.obs[Y_col], original_height_micron, 
+                                                      mosadata.obs[sample].ref_image.shape[0])
         else:
-            spomic.obs['Y_px'] = convert_micron2pixel(spomic.obs[spomic.__centroid_Y], original_height_micron, 
-                                                      spomic.ref_image.shape[0])
+            mosadata.obs[sample]['Y_px'] = convert_micron2pixel(mosadata.obs[mosadata.__centroid_Y], original_height_micron, 
+                                                      mosadata.obs[sample].ref_image.shape[0])
         logging.warning(f'X_px and Y_px are added to meta_vars')
+
+
+###
+from pandas.api.types import is_categorical_dtype, is_numeric_dtype
+from matplotlib.colors import Normalize, NoNorm, to_rgba, to_rgb, Colormap, ListedColormap
+from matplotlib.cm import ScalarMappable
+
+def savefig(fig, save):
+    # if only filename is given, add root_fig, convenient to save plots less verbose.
+#     if save == os.path.basename(save):
+#         save = os.path.join(plt.rcParams['savefig.directory'], save)
+    fig.savefig(save)
+    print(f'Figure saved at: {save}')
+# the script below implemented the network visualisation using plotting script from ATHENA
+def custom_spatial(mosadata:MOSADATA, spl: str, attr: str, *, mode: str = 'scatter', node_size: float = 4, coordinate_keys: list = ['x', 'y'],
+            mask_key: str = 'cellmasks', graph_key: str = 'knn', edges: bool = False, edge_width: float = .5,
+            edge_color: str = 'black', edge_zorder: int = 2, background_color: str = 'white', ax: plt.Axes = None,
+            norm=None, set_title: bool = True, cmap=None, cmap_labels: list = None, cbar: bool = True,
+            cbar_title: bool = True, show: bool = True, save: str = None, tight_layout: bool = True):
+    """Various functionalities to visualise samples.
+    Allows to visualise the samples and color observations according to features in either so.X or so.obs by setting the ``attr`` parameter accordingly.
+    Furthermore, observations (cells) within a sample can be quickly visualised using scatter plots (requires extraction of centroids with :func:`~.extract_centroids`)
+    or by their actual segmentatio mask by setting ``mode`` accordingly.
+    Finally, the graph representation of the sample can be overlayed by setting ``edges=True`` and specifing the ``graph_key`` as in ``mosadata.G[spl][graph_key]``.
+    Args:
+        mosadata: MOSADATA instance
+        spl: sample to visualise
+        attr: feature to visualise
+        mode: {scatter, mask}. In `scatter` mode, observations are represented by their centroid, in `mask` mode by their actual segmentation mask
+        node_size: size of the node when plotting the graph representation
+        coordinate_keys: column names in SpatialOmics.obs[spl] that indicates the x and y coordinates
+        mask_key: key for the segmentation masks when in `mask` mode
+        graph_key: which graph representation to use
+        edges: whether to plot the graph or not
+        edge_width: width of edges
+        edge_color: color of edges as string
+        edge_zorder: z-order of edges
+        background_color: background color of plot
+        ax: axes object in which to plot
+        norm: normalisation instance to normalise the values of `attr`
+        set_title: title of plot
+        cmap: colormap to use
+        cmap_labels: colormap labels to use
+        cbar: whether to plot a colorbar or not
+        cbar_title: whether to plot the `attr` name as title of the colorbar
+        show: whether to show the plot or not
+        save: path to the file in which the plot is saved
+        tight_layout: whether to apply tight_layout or not.
+    Examples:
+        .. code-block:: python
+            so = sh.dataset.imc()
+            sh.pl.spatial(so, 'slide_7_Cy2x2', 'meta_id', mode='mask')
+            sh.pl.spatial(so, 'slide_7_Cy2x2', 'meta_id', mode='scatter', edges=True)
+    .. _tutorial: https://ai4scr.github.io/ATHENA/source/tutorial.html
+    """
+    # get attribute information
+    data = None  # pd.Series/array holding the attr for colormapping
+    colors = None  # array holding the colormappig of data
+
+    # try to fetch the attr data
+    if attr:
+        if attr in mosadata.obs[spl].columns:
+            data = mosadata.obs[spl][attr]
+        elif attr in mosadata.X[spl].columns:
+            data = mosadata.X[spl][attr]
+        else:
+            raise KeyError(f'{attr} is not an column of X nor obs')
+    else:
+        colors = 'black'
+        cmap = 'black'
+        cbar = False
+
+    # broadcast if necessary
+    _is_categorical_flag = is_categorical_dtype(data)
+
+    loc = mosadata.obs[spl][coordinate_keys].copy()
+
+    # set colormap
+    if cmap is None:
+#         print('The function did not provide cmap')
+        cmap, cmap_labels = get_cmap(mosadata, attr, data)
+#         print(cmap_labels)
+    elif isinstance(cmap, str) and colors is None:
+#         print(cmap)
+        cmap = plt.get_cmap(cmap)
+#     print('cmap', cmap)
+    # normalisation
+    if norm is None:
+        if _is_categorical_flag:
+            norm = NoNorm()
+        else:
+            norm = Normalize()
+
+    # generate figure
+    if ax:
+        fig = ax.get_figure()
+        show = False  # do not automatically show plot if we provide axes
+    else:
+        fig, ax = plt.subplots(dpi=dpi)
+        ax.set_aspect('equal')
+
+    # compute edge lines
+    if edges:
+        g = mosadata.G[spl][graph_key]
+        e = np.array(g.edges, dtype=type(loc.index.dtype))
+
+        tmp1 = loc.loc[e.T[0]]
+        tmp2 = loc.loc[e.T[1]]
+
+        x = np.stack((tmp1[coordinate_keys[0]], tmp2[coordinate_keys[0]]))
+        y = np.stack((tmp1[coordinate_keys[1]], tmp2[coordinate_keys[1]]))
+
+        # we have to plot sequentially nodes and edges, this takes a bit longer but is more flexible
+        im = ax.plot(x, y, linestyle='-', linewidth=edge_width, marker=None, color=edge_color, zorder=edge_zorder)
+
+    # plot
+    if mode == 'scatter':
+#         print('Mode scatter')
+        # convert data to numeric
+        data = np.asarray(
+            data) if data is not None else None  # categorical data does not work with cmap, therefore we construct an array
+        _broadcast_to_numeric = not is_numeric_dtype(data)  # if data is now still categorical, map to numeric
+
+        if _broadcast_to_numeric and data is not None:
+            if attr in mosadata.uns['cmap_labels']:
+                cmap_labels = mosadata.uns['cmap_labels'][attr]
+#                 print(cmap_labels)
+                encoder = {value: key for key, value in cmap_labels.items()}  # invert to get encoder
+#                 print(encoder)
+            else:
+                uniq = np.unique(data)
+                encoder = {i: j for i, j in zip(uniq, range(len(uniq)))}
+                cmap_labels = {value: key for key, value in encoder.items()}  # invert to get cmap_labels
+
+            data = np.asarray([encoder[i] for i in data])
+
+        # map to colors
+        if colors is None:
+            no_na = data[~np.isnan(data)]
+            _ = norm(no_na)  # initialise norm with no NA data.
+#             print(norm(data))
+            colors = cmap(norm(data))
+#             print('Check_point cmap2colors',colors)
+
+        im = ax.scatter(loc[coordinate_keys[0]], loc[coordinate_keys[1]], s=node_size, c=colors, zorder=2.5)
+        ax.set_facecolor(background_color)
+
+    else:
+        raise ValueError(f'Invalide plotting mode {mode}')
+
+    # add colorbar
+    if cbar:
+        title = attr
+        if cbar_title is False:
+            title = ''
+        make_cbar(ax, title, norm, cmap, cmap_labels)
+
+    # format plot
+    ax_pad = min(loc[coordinate_keys[0]].max() * .05, loc[coordinate_keys[1]].max() * .05, 10)
+    ax.set_xlim(loc[coordinate_keys[0]].min() - 0.05, loc[coordinate_keys[0]].max() + 0.05)
+    ax.set_ylim(loc[coordinate_keys[1]].min() - 0.05, loc[coordinate_keys[1]].max() + 0.05)
+#     ax.legend(prop=dict(size=30))
+    ax.set_xticks([]);
+    ax.set_yticks([])
+#     ax.set_xlabel('spatial x', label_fontdict)
+#     ax.set_ylabel('spatial y', label_fontdict)
+    ax.set_aspect(1)
+    if set_title:
+        title = f'{spl}' if cbar else f'{spl}, {attr}'
+        ax.set_title(title, {'size': 15})
+
+    if tight_layout:
+        fig.tight_layout()
+
+    if show:
+        fig.show()
+
+    if save:
+        savefig(fig, save)
+    
+
+def get_cmap(mosadata, attr: str, data):
+    '''
+    Return the cmap and cmap labels for a given attribute if available, else a default
+    Parameters
+    ----------
+    so: IMCData
+        so object form which to fetch the data
+    spl: str
+        spl for which to get data
+    attr: str
+        attribute for which to get the cmap and cmap labels if available
+    Returns
+    -------
+    cmap and cmap labels for attribute
+    '''
+
+    # TODO: recycle cmap if more observations than colors
+    cmap, cmap_labels = None, None
+    if attr in mosadata.uns['cmaps'].keys():
+        cmap = mosadata.uns['cmaps'][attr]
+    elif is_categorical_dtype(data):
+        cmap = mosadata.uns['cmaps']['category']
+    else:
+        cmap = mosadata.uns['cmaps']['default']
+#     print(attr)
+#     print(so.uns['cmap_labels'])
+    if attr in mosadata.uns['cmap_labels'].keys():
+        cmap_labels = mosadata.uns['cmap_labels'][attr]
+#     print(cmap, cmap_labels)
+    return cmap, cmap_labels
+###
+
+def make_cbar(ax, title, norm, cmap, cmap_labels, im=None, prefix_labels=True):
+    """Generate a colorbar for the given axes.
+
+    Parameters
+    ----------
+    ax: Axes
+        axes for which to plot colorbar
+    title: str
+        title of colorbar
+    norm:
+        Normalisation instance
+    cmap: Colormap
+        Colormap
+    cmap_labels: dict
+        colorbar labels
+
+    Returns
+    -------
+
+    """
+    # NOTE: The Linercolormap ticks can only be set up to the number of colors. Thus if we do not have linear, sequential
+    # values [0,1,2,3] in the cmap_labels dict this will fail. Solution could be to remap.
+
+    inset = ax.inset_axes(cbar_inset)
+    fig = ax.get_figure()
+    if im is None:
+        cb = fig.colorbar(ScalarMappable(norm=norm, cmap=cmap), cax=inset)
+    else:
+        cb = fig.colorbar(im, cax=inset)
+
+    cb.ax.set_title(title, loc='left', fontdict=cbar_titel_fontdict)
+    if cmap_labels:
+        if prefix_labels:
+            labs = [f'{key}, {val}' for key, val in cmap_labels.items()]
+        else:
+            labs = list(cmap_labels.values())
+        cb.set_ticks(list(cmap_labels.keys()))
+        cb.ax.set_yticklabels(labs, fontdict=cbar_labels_fontdict)
+    else:
+        cb.ax.tick_params(axis='y', labelsize=cbar_labels_fontdict['size'])
