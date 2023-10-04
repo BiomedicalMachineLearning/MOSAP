463ab660299290b1b0792c691b9f720728d69fdc
Minh Tran <minhtran@Minhs-MacBook-Pro.local>
2023-10-04 15:52:35 +1100

    update neighourhood enrichment scoring

--- src/mosap/neighborhood.py
+++ src/mosap/neighborhood.py
@@ -10,6 +10,56 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
+GRAPH_BUILDERS = {
+    'knn': KNNGraphBuilder,
+    'Delaunay': DelaunaytGraphBuilder,
+    'radius': RadiusGraphBuilder
+}
+GRAPH_BUILDER_DEFAULT_PARAMS = {
+    'knn': {'builder_params': {'n_neighbors': 6, 'mode':'connectivity', 'metric':'minkowski', 'p':2, 'metric_params':None, 'include_self':True, 'n_jobs':-1}},
+    'Delaunay': {},
+    'radius': {'builder_params': {'radius': 36, 'mode':'connectivity', 'metric':'minkowski', 'p':2, 'metric_params':None, 'include_self':True, 'n_jobs':-1}}
+}
+def build_graph(mosadata:MOSADATA, sample: str, builder_type='knn', mask_key='cellmasks', key_added=None, config=None, inplace=True,
+                coordinate_keys=('x', 'y')):
+    """Build graph representation for a sample. A graph is constructed based on the provided segmentation masks
+    for the sample. For the `knn` and `radius` graph representation the centroid of each mask is used. For the `contact`
+    graph representation the segmentation masks are dilation_ is performed. The segmentation masks that overlap after
+    dilation are considered to be in physical contact and connected in the `contact` graph.
+
+    Args:
+        mosadata: MOSADATA object
+        sample: sample name 
+        builder_type: graph type to construct {knn, radius, Delaunay}
+        key_added: key added in mosadata.G[spl][key_add] to store the graph.
+        config: dict containing a dict 'builder_params' that specifies the graph construction parameters
+        inplace: whether to return a new SpatialOmics instance
+        coordinate_keys: column names of the x and y coordinates of a observation
+
+    """
+    if builder_type not in GRAPH_BUILDERS:
+        raise ValueError(f'invalid type {builder_type}. Available types are {GRAPH_BUILDERS.keys()}')
+    if config is None:
+        config = GRAPH_BUILDER_DEFAULT_PARAMS[builder_type].copy()
+    if key_added is None:
+        key_added = builder_type
+
+    
+    
+    g = GRAPH_BUILDERS[builder_type]
+
+    if 'include_self' in config['builder_params'] and config['builder_params'][
+        'include_self'] and builder_type == 'contact':
+        edge_list = [(i, i) for i in g.nodes]
+        g.add_edges_from(edge_list)
+
+    mosadata = mosadata if inplace else mosadata.copy()
+    if sample in mosadata.G:
+        mosadata.G[sample].update({key_added: g})
+    else:
+        mosadata.G[sample] = {key_added: g}
+
+
 class Interactions:
     """
     Estimator to quantify interaction strength between different species in the sample.
